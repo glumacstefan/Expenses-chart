e855cade0e038c5a7aafab7ed54e7f1a46b2c52f
Stefan Glumac <stefanactor94@gmail.com>
2023-09-24 20:55:16 +0200

    Delete chart cells from html and add them dynamically from Javascript

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,20 @@ const data = fetch("data.json")
 	.then(response => response.json())
 	.then(data => {
 		console.log(data);
+		return data;
 	})
 	.catch(error => console.log(error));
+
+let chartContainer = document.querySelector(".js-chart__container");
+
+data.then(data => {
+	data.forEach(element => {
+		console.log(element);
+
+		let chartCell = document.createElement("div");
+		chartCell.classList.add("chart__cell");
+		chartCell.innerHTML = `<div class="chart__bar"></div><p class="chart__label">${element.day}</p>`;
+
+		chartContainer.appendChild(chartCell);
+	});
+});
