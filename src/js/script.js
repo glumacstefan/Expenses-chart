5d7491c6111e265eaaa0668ff94e891ebb6c24fd
Stefan Glumac <stefanactor94@gmail.com>
2023-09-29 20:02:51 +0200

    Move all lines of code related to chart bar animation inside the timeout function

--- src/js/script.js
+++ src/js/script.js
@@ -2,33 +2,33 @@ const overlayer = document.querySelector(".js-overlayer");
 
 setTimeout(() => {
 	overlayer.style.display = "none";
-}, "2000");
 
-const data = fetch("data.json")
-	.then(response => response.json())
-	.then(data => {
-		console.log(data);
-		return data;
-	})
-	.catch(error => console.log(error));
+	const data = fetch("data.json")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			return data;
+		})
+		.catch(error => console.log(error));
 
-let chartContainer = document.querySelector(".js-chart__container");
+	let chartContainer = document.querySelector(".js-chart__container");
 
-data.then(data => {
-	data.forEach(element => {
-		console.log(element);
+	data.then(data => {
+		data.forEach(element => {
+			console.log(element);
 
-		let chartCell = document.createElement("div");
+			let chartCell = document.createElement("div");
 
-		chartCell.classList.add("chart__cell");
-		chartCell.innerHTML = `
-		<div class="chart__amount">$${element.amount}</div>
-			<div class="chart__cell-wrapper">
-			<div class="chart__bar" style="height:${element.amount * 2.5}px">
-			</div>
-			<p class="chart__label">${element.day}</p>
-			</div>`;
+			chartCell.classList.add("chart__cell");
+			chartCell.innerHTML = `
+	<div class="chart__amount">$${element.amount}</div>
+		<div class="chart__cell-wrapper">
+		<div class="chart__bar" style="height:${element.amount * 2.5}px">
+		</div>
+		<p class="chart__label">${element.day}</p>
+		</div>`;
 
-		chartContainer.appendChild(chartCell);
+			chartContainer.appendChild(chartCell);
+		});
 	});
-});
+}, "2000");
